2c963b8151c27ec01af67baaf9edb9880f9ec11a
Artem <rozen19942810@gmail.com>
2020-12-27 00:38:22 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import React from "react";
 import {Posts} from "./components/Posts";
 import {FetchedPosts} from "./components/FetchedPosts";
 import PostForm from "./components/PostForm";
+import {useSelector} from "react-redux";
+import {Loader} from "./assets/Loader";
 
 function App() {
+
+    const {loading} = useSelector(state => state.app.loading)
+
+    if (loading) return <Loader/>
+
     return (
         <div className='container pt-3'>
             <div className='row'>
