91252cc99cafd27f4c336924aeeab84986715df5
Artem <rozen19942810@gmail.com>
2020-12-27 17:31:50 +0200

    added middleware for ban forbidden words

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import thunk from 'redux-thunk'
 import {postsReducer} from "./postsReducer";
 import {appReducer} from "./appReducer";
+import {forbiddenWordsMiddleware} from "./middleware";
 
 const reducers = combineReducers({
     posts: postsReducer,
@@ -9,6 +10,6 @@ const reducers = combineReducers({
 })
 
 export const store = createStore(reducers, compose(
-    applyMiddleware(thunk),
+    applyMiddleware(thunk, forbiddenWordsMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
